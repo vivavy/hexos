a2f7c350b5ca6214b9d84722d486853f12f33efb
vivavy <vavylonessins@vk.com>
2024-04-24 13:28:06 +0300

    bugfixes

--- src/kernel/src/main.cpp
+++ src/kernel/src/main.cpp
@@ -7,9 +7,12 @@
 #include <types.hpp>
 #include <cpu.hpp>
 #include <fb.hpp>
+#include <mmu.hpp>
 
 nomangle
 void _start(void *bootinfo) {
+    void *pages = mmu::palloc(10);  // allocate 10 blocks (1 block = 16 bytes)
+    mmu::pfree(pages, 10);  // free 10 blocks
     fb::teletype::init();
     fb::teletype::putString("\e02mHello, World!\e07m");
     halt();
